c103993ecb37845b6c468de21d8502b79b95c288
antikantian <lesterm@mac.com>
2018-02-03 00:48:10 -0500

    Parity transaction trace

--- src/objects/mod.rs
+++ src/objects/mod.rs
@@ -2,6 +2,9 @@ mod block;
 mod log;
 mod transaction;
 
+pub mod trace;
+
 pub use self::block::Block;
 pub use self::log::Log;
+pub use self::trace::ParityTrace;
 pub use self::transaction::Transaction;
---
+++ src/objects/trace.rs
@@ -0,0 +1,38 @@
+use types::{H160, H256, U256};
+
+#[derive(Serialize, Deserialize)]
+pub enum CallType {
+    Call
+}
+
+#[derive(Serialize, Deserialize)]
+#[serde(rename_all="camelCase")]
+pub struct ParityTrace {
+    pub action: Action,
+    pub block_hash: H256,
+    pub block_number: u64,
+    pub result: ActionResult,
+    pub sub_traces: u8,
+    pub trace_address: Vec<u8>,
+    pub transaction_hash: H256,
+    pub transaction_position: u64,
+    pub action_type: CallType
+}
+
+#[derive(Serialize, Deserialize)]
+#[serde(rename_all="camelCase")]
+pub struct Action {
+    pub call_type: CallType,
+    pub from: H160,
+    pub gas: U256,
+    pub input: String,
+    pub to: H160,
+    pub value: U256
+}
+
+#[derive(Serialize, Deserialize)]
+#[serde(rename_all="camelCase")]
+pub struct ActionResult {
+    pub gas_used: U256,
+    pub output: String
+}
