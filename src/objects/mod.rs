cfcdc9769bba554fbb9a16e0366836c771fa3675
antikantian <lesterm@mac.com>
2018-02-04 19:48:37 -0500

    Exposed typeclasses

--- src/objects/mod.rs
+++ src/objects/mod.rs
@@ -5,6 +5,11 @@ mod transaction;
 pub mod trace;
 
 pub use self::block::Block;
-pub use self::log::Log;
+pub use self::log::{Log, LogLike};
 pub use self::trace::ParityTrace;
-pub use self::transaction::{Transaction, TransactionReceipt};
+pub use self::transaction::{
+    Transaction,
+    TransactionLike,
+    TransactionReceipt,
+    ReceiptLike
+};
