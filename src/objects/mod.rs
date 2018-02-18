143b1d342c8457ab59de0d451799716da88688c9
antikantian <lesterm@mac.com>
2018-02-17 21:12:54 -0500

    Added `TransactionCall`

--- src/objects/mod.rs
+++ src/objects/mod.rs
@@ -6,6 +6,7 @@ mod transaction;
 pub mod trace;
 
 pub use self::block::Block;
+pub use self::call::TransactionCall;
 pub use self::log::{Log, LogLike};
 pub use self::trace::ParityTrace;
 pub use self::transaction::{
